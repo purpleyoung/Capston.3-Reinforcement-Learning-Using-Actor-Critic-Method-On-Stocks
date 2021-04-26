98a5c5888921285d4da397ebef113d2782268cdf
333Kenji <333kenji@gmail.com>
2021-04-26 15:03:35 -0700

    we have programmable output!

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ import datetime
 import pandas_datareader.data as web
 
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import mpld3
+from mpld3 import plugins
+np.random.seed(9615)
+
 # Layout
 app = dash.Dash()
 app.title = "ML Sock Predict"
@@ -28,12 +35,44 @@ def update_value(input_data):
     start = datetime.datetime(2015, 1, 1)
     end = datetime.datetime.now()
     df = web.DataReader(input_data, 'yahoo', start, end)
-    return dcc.Graph(id="demo", figure={'data': [{'x': df.index, 'y': df.Close, 'type': 'line', 'name': input_data}, ], 'layout': {'title': input_data}})
+    #return html(df)
+    
+    # generate df
+    N = 100
+    df = pd.DataFrame((.1 * (np.random.random((N, 5)) - .5)).cumsum(0),
+                      columns=['a', 'b', 'c', 'd', 'e'],)
+    
+    # plot line + confidence interval
+    fig, ax = plt.subplots()
+    ax.grid(True, alpha=0.3)
+    
+    for key, val in df.iteritems():
+        l, = ax.plot(val.index, val.values, label=key)
+        ax.fill_between(val.index,
+                        val.values * .5, val.values * 1.5,
+                        color=l.get_color(), alpha=.4)
+    
+    # define interactive legend
+    
+    handles, labels = ax.get_legend_handles_labels() # return lines and labels
+    interactive_legend = plugins.InteractiveLegendPlugin(zip(handles,
+                                                             ax.collections),
+                                                         labels,
+                                                         alpha_unsel=0.5,
+                                                         alpha_over=1.5, 
+                                                         start_visible=True)
+    plugins.connect(fig, interactive_legend)
+    
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('Interactive legend', size=20)
+    
+    mpld3.show()
+    return dcc.Graph(id="demo", figure={mpld3.show()})
 
 
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0', port=8000, debug=True)
 
-    
     
     
