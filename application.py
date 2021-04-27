b9775f673c828def5be37dd0e8f89dc6436e3994
333Kenji <333kenji@gmail.com>
2021-04-27 14:29:37 -0700

    testing d template

--- application.py
+++ application.py
@@ -1,33 +1,29 @@
+import os
+
 import dash
-import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 
-USERNAME_PASSWORD_PAIRS = [
-    ['JamesBond', '007'],['LouisArmstrong', 'satchmo']
-]
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash()
-auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
-server = app.server
+application = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    dcc.RangeSlider(
-        id='range-slider',
-        min=-5,
-        max=6,
-        marks={i:str(i) for i in range(-5, 7)},
-        value=[-3, 4]
+server = application.server
+
+application.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
     ),
-    html.H1(id='product')  # this is the output
-], style={'width':'50%'})
+    html.Div(id='display-value')
+])
 
-@app.callback(
-    Output('product', 'children'),
-    [Input('range-slider', 'value')])
-def update_value(value_list):
-    return value_list[0]*value_list[1]
+@application.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
-    app.run_server(debug = True)
+    application.run_server(debug=True)
