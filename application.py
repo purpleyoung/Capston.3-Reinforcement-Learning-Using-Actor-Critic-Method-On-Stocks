d59848a03b30f4da8f65c232002ec1e18a1ce673
333Kenji <333kenji@gmail.com>
2021-04-27 16:56:14 -0700

    8080

--- application.py
+++ application.py
@@ -1,31 +1,30 @@
-import os
-
 import dash
 import dash_core_components as dcc
-import dash_html_components as html
-
+import dash_html_components as html########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-server = app.server
-
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
-])
-
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
-
+application = app.server
+app.title='Dash on AWS EB!'########### Set up the layout
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+    html.Div(children='''
+            This is Dash running on Elastic Beanstalk.
+        '''),
+    dcc.Graph(
+            id='example-graph',
+            figure={
+                'data': [
+                    {'x': ['left', 'center', 'right'], 'y': [3,7,6], 'type': 'bar', 'name': 'category 1'},
+                    {'x': ['left', 'center', 'right'], 'y': [4,2,5], 'type': 'bar', 'name': 'category 2'},
+                ],
+                'layout': {
+                    'plot_bgcolor': 'lightgray',
+                    'title': 'Graph Title',
+                    'xaxis':{'title':'x-axis label'},
+                    'yaxis':{'title':'y-axis label'},
+                },
+            }
+        )
+])########### Run the app
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=80, debug=True)
-
-    
+    application.run(debug=True, port=8080)
