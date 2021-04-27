fc09541aee991583a91ce84158807a7bde4af2e0
333Kenji <333kenji@gmail.com>
2021-04-27 16:18:46 -0700

    add a route

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
-
+@app.route('/')
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(
@@ -27,3 +27,5 @@ def display_value(value):
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=5000, debug=True)
+
+    
