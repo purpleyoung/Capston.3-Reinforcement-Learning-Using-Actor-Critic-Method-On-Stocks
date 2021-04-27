25db806e83ca21da16bf8725170fd3a82e315ccd
333Kenji <333kenji@gmail.com>
2021-04-27 16:38:02 -0700

    80

--- application.py
+++ application.py
@@ -26,6 +26,6 @@ def display_value(value):
     return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=5000, debug=True)
+    app.run_server(host='0.0.0.0', port=80, debug=True)
 
     
