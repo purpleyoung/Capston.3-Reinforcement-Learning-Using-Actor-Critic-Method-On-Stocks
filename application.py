d6a3a551c1f866efb0b56d1a9b7cc88edc474f49
333Kenji <333kenji@gmail.com>
2021-04-27 13:33:09 -0700

    stylesheets halfway out

--- application.py
+++ application.py
@@ -42,7 +42,7 @@ from mpld3 import plugins
 
 
 # Init
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 application = app.server
