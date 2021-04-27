a2a8166f0e960bdcf43ac2874939c64cab337d87
333Kenji <333kenji@gmail.com>
2021-04-27 14:17:28 -0700

    an Actual D template..

--- application.py
+++ application.py
@@ -1,35 +1,33 @@
-from flask import Flask
+import dash
+import dash_auth
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
+USERNAME_PASSWORD_PAIRS = [
+    ['JamesBond', '007'],['LouisArmstrong', 'satchmo']
+]
 
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+app = dash.Dash()
+auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
+server = app.server
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
-app = application
+app.layout = html.Div([
+    dcc.RangeSlider(
+        id='range-slider',
+        min=-5,
+        max=6,
+        marks={i:str(i) for i in range(-5, 7)},
+        value=[-3, 4]
+    ),
+    html.H1(id='product')  # this is the output
+], style={'width':'50%'})
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+@app.callback(
+    Output('product', 'children'),
+    [Input('range-slider', 'value')])
+def update_value(value_list):
+    return value_list[0]*value_list[1]
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
-
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
-    application.run()
+if __name__ == '__main__':
+    app.run_server(debug = True)
